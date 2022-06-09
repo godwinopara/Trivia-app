4ac15f68484d05d0e369a44a2d6a488f4af056e9
godwin opara <godwinopara62@yahoo.com>
2022-06-09 13:23:10 +0100

    added the api route for a get request to /questions and /categories endpoints

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,6 +9,17 @@ from models import setup_db, Question, Category
 QUESTIONS_PER_PAGE = 10
 
 
+def paginated_questions(selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = page + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -17,17 +28,34 @@ def create_app(test_config=None):
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, Authorization, true")
+
+        response.headers.add("Access-Control-Allow-Methods",
+                             "GET, PATCH, POST, DELETE, OPTION")
+        return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/api/categories')
+    def get_categories():
+        categories = Category.query.all()
+        formated_categories = [category.format() for category in categories]
 
+        return jsonify({
+            "success": True,
+            "total_categories": len(formated_categories)
+        })
     """
     @TODO:
     Create an endpoint to handle GET requests for questions,
@@ -40,6 +68,20 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+
+    @app.route('/api/questions')
+    def get_questions():
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginated_questions(selection)
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify({
+            "success": True,
+            "questions": current_questions,
+            "total_question": len(current_questions)
+        })
 
     """
     @TODO:
